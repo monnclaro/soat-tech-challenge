db5fb2eee3306f64ef861c3ecb6fbf0c0cd8bbe9
Augusto Henrique de Farias Menna Barreto Monclaro <monclaro.a@gmail.com>
2026-04-07 21:46:02 -0300

    fix: Ajustando startup

--- SoatTechChallenge.Infrastructure/DependencyInjection.cs
+++ SoatTechChallenge.Infrastructure/DependencyInjection.cs
@@ -41,15 +41,4 @@ public static class DependencyInjection
 
         return services;
     }
-
-    public static async Task InitializeDatabaseAsync(this WebApplication app)
-    {
-        using var scope = app.Services.CreateScope();
-
-        var db = scope.ServiceProvider.GetRequiredService<SoatTechChallengeDbContext>();
-        await db.Database.MigrateAsync();
-
-        var seeder = scope.ServiceProvider.GetRequiredService<IDatabaseSeeder>();
-        await seeder.SeedAsync();
-    }
 }
